71cecb989f0f23558404957341adbdfdc1cad573
Quentin Lemaire <quentin.lemaire@supinfo.com>
2017-04-01 03:29:58 +0200

    up

--- globales.py
+++ globales.py
@@ -4,7 +4,7 @@
 # @Email:  quentin.lemaire@supinfo.com
 # @Filename: globales.py
 # @Last modified by:   SkYNewZ
-# @Last modified time: 2017-04-01T03:21:55+02:00
+# @Last modified time: 2017-04-01T03:28:59+02:00
 # @License: 214520
 # @Copyright: SUPINFO
 
